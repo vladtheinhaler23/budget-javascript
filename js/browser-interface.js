5c0b4e35324c4618c29b67fa22be799bbc69f552
Garrett Biernat <garrett@gmail.com>
2016-06-22 14:27:08 -0700

    merged:

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,26 +1,26 @@
 var checkLoginState = require('./../js/facebook-backend.js').checkLoginState;
 var moment = require('moment');
-var loading_screen = pleaseWait({
-  logo: "pictures/logo.png",
-  backgroundColor: '#43a047',
-  loadingHtml: "<div class='birthday'><h2 id='message'>You Must be at least 18 Years Old to view this page</h2><div id='dateinput'><label for='birthday'>Enter Birthday</label><input type='date' class='datepicker' id='birthday'><a class='waves-effect waves-light btn-large' id='submit'>Submit</a></div></div>"
-});
-$('.datepicker').pickadate({
-  selectMonths: true, // Creates a dropdown to control month
-  selectYears: 70 // Creates a dropdown of 15 years to control year
-});
-$("#submit").click(function(){
-  var birthdate = $("#birthday").val();
-  var date = new Date(birthdate);
-  var dob = moment(date).format("YYYY-MM-DD");
-  var age = moment().diff(moment(dob, 'YYYYMMDD'), 'years');
-  if(age >=18){
-    loading_screen.finish();
-  }else{
-    $("#message").text("You are under 18 and not allowed to view this site please leave");
-    $("#dateinput").fadeOut();
-  }
-})
+// var loading_screen = pleaseWait({
+//   logo: "pictures/logo.png",
+//   backgroundColor: '#43a047',
+//   loadingHtml: "<div class='birthday'><h2 id='message'>You Must be at least 18 Years Old to view this page</h2><div id='dateinput'><label for='birthday'>Enter Birthday</label><input type='date' class='datepicker' id='birthday'><a class='waves-effect waves-light btn-large' id='submit'>Submit</a></div></div>"
+// });
+// $('.datepicker').pickadate({
+//   selectMonths: true, // Creates a dropdown to control month
+//   selectYears: 70 // Creates a dropdown of 15 years to control year
+// });
+// $("#submit").click(function(){
+//   var birthdate = $("#birthday").val();
+//   var date = new Date(birthdate);
+//   var dob = moment(date).format("YYYY-MM-DD");
+//   var age = moment().diff(moment(dob, 'YYYYMMDD'), 'years');
+//   if(age >=18){
+//     loading_screen.finish();
+//   }else{
+//     $("#message").text("You are under 18 and not allowed to view this site please leave");
+//     $("#dateinput").fadeOut();
+//   }
+// })
 function display(response){
   console.log(response);
   if(response.birthday != null){
