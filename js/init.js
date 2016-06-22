c68a3964cc1fd61e05ae045a8cf0195ca2bd3acd
Garrett Biernat <garrett@gmail.com>
2016-06-22 14:05:41 -0700

    datepicker

--- js/init.js
+++ js/init.js
@@ -16,4 +16,8 @@ $(document).ready(function(){
       });
       $(document).ready(function() {
         $('select').material_select();
-      }); 
+      });
+      $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 70 // Creates a dropdown of 15 years to control year
+      });
