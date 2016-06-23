59d560c2c61c027a6d51c2dad7c20fcdf94e5a97
Joshua Logan <joshlogan32323@gmail.com>
2016-06-22 21:34:23 -0700

    put user transactions into materialize cards

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,5 @@
+var createUserCard = require('./../js/recent-transaction-interface.js').createUserCard;
+
 var config = {
  apiKey: "AIzaSyDAazSLzo-KZeqTds-eWeut4a40Tr8-kxc",
  authDomain: "budget-1617b.firebaseapp.com",
@@ -23,9 +25,8 @@ exports.getUserTransactions = function(userId) {
   firebase.database().ref('/users/' + userId + "/transactions").once('value').then(function(snapshot) {
   var userTransactions = snapshot.val();
   console.log(userTransactions);
-  userTransactions.forEach(function(transaction) {
-    $("#recents").append("<p>" + "Date: " + transaction.date + "<br>" + "Amount: " + transaction.amount + "<br>" + "Dispensary: " + transaction.dispensary + "</p>");
-  })
+  createUserCard(userTransactions);
+
 
 });
 };
---
+++ js/recent-transaction-interface.js
@@ -0,0 +1,21 @@
+
+exports.createUserCard = function(userTransactions) {
+  userTransactions.forEach(function(transaction) {
+    $("#recents").append(
+  "<div class='col s3 m3'>" +
+    "<div class='card'>" +
+      "<div class='card-image waves-effect waves-block waves-light'>" +
+        "<img class='activator' src='images/office.jpg'>" +
+      "</div>" +
+      "<div class='card-content'>" +
+        "<span class='card-title activator grey-text text-darken-4'>" + transaction.amount + "<br>" + transaction.date + "<br>" + transaction.strain + "<i class='material-icons right'>more_vert</i></span>" +
+        "<p><a href='#'>This is a link</a></p>" +
+      "</div>" +
+      "<div class='card-reveal'>" +
+        "<span class='card-title grey-text text-darken-4'>Transaction Details<i class='material-icons right'>close</i></span>" +
+        "<p>Date: " + transaction.date + "<br>" + "Amount: " + transaction.amount + "<br>" + "Dispensary: " + transaction.dispensary + "<br>" + transaction.location + "</p>" +
+      "</div>" +
+    "</div>" +
+  "</div>");
+  });
+};
